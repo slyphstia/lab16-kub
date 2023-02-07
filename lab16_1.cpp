306f1b94e7c1200bad829886c792d013f69f994e
slyphstia <apiwit_boonyarit@cmu.ac.th>
2023-02-07 17:19:55 +0700

    complete

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -2,11 +2,26 @@
 using namespace std;
 
 int main(){
-int a =5 ;
-string b = "A";
+	int a = 5;
+	string b = "A";
+	string &c = b ;
+	int *x = &a ;
+	string *y = &b ;
+	int **z = &x ;
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl ;
+	cout << &a << " " << &b << " " << &c << " " << &x << " " << &y << " " << &z << endl ;
 
+	c = "F" ;
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl ;
+	
+	*y = "W" ;
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl ;
 
-	
-	
+	*x = 6;
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl ;
+
+	**z = 7;
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl ;
+
 	return 0;
 }
